e7ea31c3b4fb7a717387c3700fc2a99b7797715f
Unknown <thomas.shamoian@student.rocleiden.nl>
2018-07-04 10:13:29 +0200

    Examen aangemaakt
    database en controllers aangemakt

---
+++ thomasshamoian/database/migrations/2018_07_04_080558_create_gebruikers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGebruikersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gebruikers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('gebruiker');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gebruikers');
+    }
+}
